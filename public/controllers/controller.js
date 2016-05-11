39ae64ff9038b3b2607bf01fbd6b67a335e1c3dc
pevargas <pevargas@valdosta.edu>
2016-05-11 13:36:19 -0400

    http request for data

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,24 +1,7 @@
 angular.module('contactListApp',[])
 .controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    //dummy data
-   person1 = {
-       name: 'Tim',
-       email:'time@gmail.com',
-       number: '(222)-222-222'
-   };
-   person2 = {
-       name: 'Kate',
-       email:'kate@gmail.com',
-       number: '(333)-333-333'
-   };
-   person3 = {
-       name: 'Jose',
-       email:'jode@gmail.com',
-       number: '(111)-111-111'
-   };
-   //adding dummy data to our array
-   var contactList = [person1,person2,person3];
-   
-   //this allow us to use the contact list in the html file using ng-repeat
-   $scope.contactList = contactList;
+    $http.get('/contactList').success(function(response){
+        console.log("test of data");
+        $scope.contactList = response;
+    });
 }]);
--- server.js
+++ server.js
@@ -6,6 +6,29 @@ var app = express();
 });*/
 
 app.use(express.static(__dirname + '/public'));
-app.listen(3000);
 
+//Setting up router for our contactList
+app.get('/contactList',function(req,res){
+    console.log("I received a GET REQUEST");
+    person1 = {
+       name: 'Tim',
+       email:'time@gmail.com',
+       number: '(222)-222-222'
+   };
+   person2 = {
+       name: 'Kate',
+       email:'kate@gmail.com',
+       number: '(333)-333-333'
+   };
+   person3 = {
+       name: 'Jose',
+       email:'jode@gmail.com',
+       number: '(111)-111-111'
+   };
+
+    var contactList = [person1,person2,person3];
+    //response to the get request see it in JSON format
+    res.json(contactList);
+ });
+app.listen(3000);
 console.log("Server running on por:3000");
