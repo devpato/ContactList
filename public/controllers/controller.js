5c79dd19d9512a1b02397948d13913f9e50628bd
pevargasg <pevargas@valdosta.edu>
2016-05-13 13:59:47 -0400

    refreshing page

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,15 +1,21 @@
 angular.module('contactListApp',[])
 .controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    $http.get('/contactList').success(function(response){
-        console.log("test of data");
-        $scope.contactList = response;
-    });
+    var refresh = function(){
+        $http.get('/contactList').success(function(response){
+            console.log("test of data");
+            $scope.contactList = response;
+            $scope.contact = "" ;
+        });
+    };
+    refresh();
     
+
     $scope.addConctact = function () {
         console.log($scope.contact);
         //this sends the input data to the server
         $http.post('/contactList',$scope.contact).success(function(response){
             console.log(response);
+            refresh();
         });       
         
     };
